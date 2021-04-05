abc9fe9e0c00198c8c3e9e32c3d0e0c0ae859b09
Nikita <main.acr0matic@gmail.com>
2021-04-05 10:42:23 +0300

    feat: верстка футера

--- src/scripts/misc.js
+++ src/scripts/misc.js
@@ -9,3 +9,13 @@ if (map) {
     if (window.pageYOffset >= map.offsetTop - 2000) map.append(script);
   });
 }
+
+const scrollTopArrow = document.getElementById('scrollTop');
+if (scrollTopArrow) {
+  scrollTopArrow.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+}
