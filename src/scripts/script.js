0b8971e81168f798ba90286d47483a0ca902781d
Nikita <main.acr0matic@gmail.com>
2021-04-06 14:22:24 +0300

    feat: модальные окна, доработка стилей

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,9 @@
-/* global tippy */
+/* global tippy, MicroModal */
+
+const callbackModal = document.getElementById('modal-callback');
+
+let modalTitle = callbackModal.querySelector('.modal__title');
+let formType = '';
 
 tippy('.advantages__item', {
   placement: 'bottom',
@@ -6,4 +11,24 @@ tippy('.advantages__item', {
   maxWidth: 380,
   theme: 'children',
   arrow: false,
+});
+
+MicroModal.init({
+  awaitCloseAnimation: true,
+  disableFocus: true,
+});
+
+const signButtons = document.querySelectorAll('[data-sign-type]');
+signButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    formType = button.getAttribute('data-sign-type');
+    modalTitle.innerHTML = button.getAttribute('data-modal-title');
+  });
+});
+
+const modalButtons = document.querySelectorAll('[data-micromodal-trigger]:not([data-modal-title])');
+modalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    modalTitle.innerHTML = callbackModal.getAttribute('data-modal-title');
+  });
 });
