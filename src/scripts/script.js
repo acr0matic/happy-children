4b7e0f1de955ea0ee8a25cad2c2a86fd8b632357
Nikita <main.acr0matic@gmail.com>
2021-04-27 16:52:07 +0300

    feat: доработка мобильной версии, меню

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -37,7 +37,18 @@ modalButtons.forEach((button) => {
 // eslint-disable-next-line no-restricted-globals
 SmoothScroll();
 
-const mobileButton = document.getElementById('mobile-button');
-mobileButton.addEventListener('click', () => {
-  mobileButton.classList.toggle('is-active');
+/* Логика для мобильного меню */
+const headerMobile = document.querySelector('.header .header__mobile');
+const mobileMenuButton = headerMobile.querySelector('.hamburger');
+const mobileMenu = document.getElementById('mobile-menu');
+const mobileOverlay = mobileMenu.querySelector('.mobile-menu__overlay');
+
+mobileMenuButton.addEventListener('click', () => {
+  mobileMenuButton.classList.toggle('is-active');
+  mobileMenu.classList.toggle('mobile-menu--open');
+});
+
+mobileOverlay.addEventListener('click', () => {
+  mobileMenuButton.classList.remove('is-active');
+  mobileMenu.classList.remove('mobile-menu--open');
 });
