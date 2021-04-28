70aa82d0a8f0539d7c5750ad40a0c2d73c785eb4
Nikita <main.acr0matic@gmail.com>
2021-04-28 15:34:35 +0300

    feat: страница "наша команда"

---
+++ src/team.php
@@ -0,0 +1,159 @@
+<?php include_once('partials/header.php') ?>
+
+<!-- Основной контент -->
+<main>
+  <section id="team">
+    <div class="container">
+      <div class="team">
+        <div class="team__hero">
+          <h1 class="team__title page__title">Наши педагоги</h1>
+          <picture class="team__picture">
+            <source srcset="img/page/team/hero__image.webp" type="image/webp">
+            <img src="img/page/team/hero__image.img" alt="" class="team__image">
+          </picture>
+          <!-- /.team__picture -->
+
+          <div class="shape">
+            <svg>
+              <clipPath id="hero-wave" clipPathUnits="objectBoundingBox">
+                <path d="M 0 1 L 0 0 L 1 0 L 1 0.593 C 0.644 0.515 0.59 1.083 0 0.894" />
+              </clipPath>
+            </svg>
+          </div>
+        </div>
+        <!-- /.team__hero -->
+
+        <div class="team__list">
+          <div class="team__item">
+            <div class="row align-items-center">
+              <div class="col-12 col-lg-5 col-xl-6 mb-4 mb-lg-0">
+                <div class="team__photo mx-auto mx-lg-0">
+                  <img class="team__image" src="img/page/main/about/photo.jpg" alt="">
+                </div>
+                <!-- /.about__picture -->
+              </div>
+              <!-- /.col-6 -->
+
+              <div class="col-12 col-lg-7 col-xl-6">
+                <div class="team__content">
+                  <h2 class="team__name">
+                    Елена
+                  </h2>
+
+                  <div class="team__text">
+                    <p class="team__text section__text">
+                      <strong>Педагог</strong>, преподаватель английского языка. Высшее педагогическое и психологическое образование. Прошла обучение и стажировку в США по программе английского языка «FastTracKids». Опыт работы более 12 лет. Огромное количество различных тренингов и курсов, среди которых психолого-педагогическое сопровождение развития эмоционального интеллекта детей дошкольного возраста.
+                    </p>
+                  </div>
+                  <!-- /.about__text -->
+                </div>
+                <!-- /.about__content -->
+              </div>
+              <!-- /.col-6 -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.team__item -->
+
+          <div class="team__item">
+            <div class="row align-items-center">
+              <div class="col-12 col-lg-5 col-xl-6 mb-4 mb-lg-0">
+                <div class="team__photo mx-auto mx-lg-0">
+                  <img class="team__image" src="img/page/main/about/photo.jpg" alt="">
+                </div>
+                <!-- /.about__picture -->
+              </div>
+              <!-- /.col-6 -->
+
+              <div class="col-12 col-lg-7 col-xl-6">
+                <div class="team__content">
+                  <h2 class="team__name">
+                    Елена
+                  </h2>
+
+                  <div class="team__text">
+                    <p class="team__text section__text">
+                      <strong>Педагог</strong>, преподаватель английского языка. Высшее педагогическое и психологическое образование. Прошла обучение и стажировку в США по программе английского языка «FastTracKids». Опыт работы более 12 лет. Огромное количество различных тренингов и курсов, среди которых психолого-педагогическое сопровождение развития эмоционального интеллекта детей дошкольного возраста.
+                    </p>
+                  </div>
+                  <!-- /.about__text -->
+                </div>
+                <!-- /.about__content -->
+              </div>
+              <!-- /.col-6 -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.team__item -->
+
+          <div class="team__item">
+            <div class="row align-items-center">
+              <div class="col-12 col-lg-5 col-xl-6 mb-4 mb-lg-0">
+                <div class="team__photo mx-auto mx-lg-0">
+                  <img class="team__image" src="img/page/main/about/photo.jpg" alt="">
+                </div>
+                <!-- /.about__picture -->
+              </div>
+              <!-- /.col-6 -->
+
+              <div class="col-12 col-lg-7 col-xl-6">
+                <div class="team__content">
+                  <h2 class="team__name">
+                    Елена
+                  </h2>
+
+                  <div class="team__text">
+                    <p class="team__text section__text">
+                      <strong>Педагог</strong>, преподаватель английского языка. Высшее педагогическое и психологическое образование. Прошла обучение и стажировку в США по программе английского языка «FastTracKids». Опыт работы более 12 лет. Огромное количество различных тренингов и курсов, среди которых психолого-педагогическое сопровождение развития эмоционального интеллекта детей дошкольного возраста.
+                    </p>
+                  </div>
+                  <!-- /.about__text -->
+                </div>
+                <!-- /.about__content -->
+              </div>
+              <!-- /.col-6 -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.team__item -->
+
+          <div class="team__item">
+            <div class="row align-items-center">
+              <div class="col-12 col-lg-5 col-xl-6 mb-4 mb-lg-0">
+                <div class="team__photo mx-auto mx-lg-0">
+                  <img class="team__image" src="img/page/main/about/photo.jpg" alt="">
+                </div>
+                <!-- /.about__picture -->
+              </div>
+              <!-- /.col-6 -->
+
+              <div class="col-12 col-lg-7 col-xl-6">
+                <div class="team__content">
+                  <h2 class="team__name">
+                    Елена
+                  </h2>
+
+                  <div class="team__text">
+                    <p class="team__text section__text">
+                      <strong>Педагог</strong>, преподаватель английского языка. Высшее педагогическое и психологическое образование. Прошла обучение и стажировку в США по программе английского языка «FastTracKids». Опыт работы более 12 лет. Огромное количество различных тренингов и курсов, среди которых психолого-педагогическое сопровождение развития эмоционального интеллекта детей дошкольного возраста.
+                    </p>
+                  </div>
+                  <!-- /.about__text -->
+                </div>
+                <!-- /.about__content -->
+              </div>
+              <!-- /.col-6 -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.team__item -->
+        </div>
+        <!-- /.team__list -->
+      </div>
+      <!-- /.team -->
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once('partials/footer.php') ?>
