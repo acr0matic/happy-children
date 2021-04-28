34d44770f185c01a7feae15fd8936983648c47b0
Nikita <main.acr0matic@gmail.com>
2021-04-28 19:16:29 +0300

    feat: верстка "секции"

--- src/studios.php
+++ src/studios.php
@@ -18,10 +18,10 @@
         <!-- /.studios__content -->
 
         <picture class="studios__picture">
-            <source srcset="img/page/studios/hero__image.webp" type="image/webp">
-            <img src="img/page/studios/hero__image.img" alt="" class="hero__image">
-          </picture>
-          <!-- /.team__picture -->
+          <source srcset="img/page/studios/hero__image.webp" type="image/webp">
+          <img src="img/page/studios/hero__image.img" alt="" class="hero__image">
+        </picture>
+        <!-- /.text-block__picture -->
 
         <div class="shape">
           <svg>
@@ -32,6 +32,201 @@
         </div>
       </div>
       <!-- /.team -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <section id="service" class="section">
+    <div class="container">
+      <div class="service">
+        <div class="service__item text-block__item">
+          <div class="row align-items-center">
+            <div class="col-12 col-lg-5 col-xl-6 mb-4 mb-lg-0">
+              <div class="text-block__picture mx-auto mx-lg-0">
+                <img class="text-block__image" src="img/page/main/about/photo.jpg" alt="">
+              </div>
+              <!-- /.about__picture -->
+            </div>
+            <!-- /.col-6 -->
+
+            <div class="col-12 col-lg-7 col-xl-6">
+              <div class="text-block__content">
+                <h2 class="text-block__title">
+                  Английский язык
+                </h2>
+
+                <div class="text-block__text">
+                  <p class="text-block__text section__text">
+                    Авторские занятия по изучению английского языка от <strong>Елены Левиной</strong>, педагог-психолог, преподаватель английского языка.
+                  </p>
+
+                  <p class="text-block__text section__text">
+                    Все занятия проходят в игровой форме. Каждое занятие делится на 4 блока, после каждого блока - музыкальная разминка.
+                  </p>
+
+                  <ul class="text-block__list list list--line">
+                    <li class="list__item">Круг - садимся в круг, здороваемся и знакомимся с книгой, короткую будем изучать;</li>
+                    <li class="list__item">Грамматика - знакомимся с правилами английского языка, буквами;</li>
+                    <li class="list__item">Чтение книги;</li>
+                    <li class="list__item">Творческая работа - делаем поделку или рисуем героев книги. Этим развивается мелкая моторика, что влияет на развитие умственных способностей, а также на речь.</li>
+                  </ul>
+                </div>
+                <!-- /.about__text -->
+
+                <p class="text-block__text section__text">
+                  <strong>Время проведения</strong> <br>
+                  Суббота в 10:00 <br>
+                  Рекомендуемый возраст – от 4,5 лет
+                </p>
+
+                <p class="text-block__text section__text">
+                  <strong>Стоимость абонемента</strong> из 4 занятий – 4.000 руб. <br> Группа 5-6 человек
+                </p>
+
+                <div class="text-block__action">
+                  <button class="text-block__button button button-outline" data-micromodal-trigger="modal-callback">Записаться на пробное занятие</button>
+                </div>
+                <!-- /.about__action -->
+              </div>
+              <!-- /.about__content -->
+            </div>
+            <!-- /.col-6 -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.text-block__item -->
+
+        <div class="service__item text-block__item">
+          <div class="row align-items-center">
+            <div class="col-12 col-lg-5 col-xl-6 mb-4 mb-lg-0">
+              <div class="text-block__picture mx-auto mx-lg-0">
+                <img class="text-block__image" src="img/page/main/about/photo.jpg" alt="">
+              </div>
+              <!-- /.about__picture -->
+            </div>
+            <!-- /.col-6 -->
+
+            <div class="col-12 col-lg-7 col-xl-6">
+              <div class="text-block__content">
+                <h2 class="text-block__title">
+                  Шахматы
+                </h2>
+
+                <div class="text-block__text">
+                  <ul class="text-block__list list list--line">
+                    <li class="list__item list__item--head">Занятия состоят из 4 элементов:</li>
+                    <li class="list__item">Шахматная разминка;</li>
+                    <li class="list__item">Прохождение материала;</li>
+                    <li class="list__item">Обычная разминка;</li>
+                    <li class="list__item">Игровая практика;</li>
+                  </ul>
+                </div>
+                <!-- /.about__text -->
+
+                <p class="text-block__text section__text">
+                  <strong>Стоимость абонемента</strong> из 4 занятий – 2.400 руб. <br> Группа 5-6 человек
+                </p>
+
+                <div class="text-block__action">
+                  <button class="text-block__button button button-outline" data-micromodal-trigger="modal-callback">Записаться на пробное занятие</button>
+                </div>
+                <!-- /.about__action -->
+              </div>
+              <!-- /.about__content -->
+            </div>
+            <!-- /.col-6 -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.text-block__item -->
+
+        <div class="service__item text-block__item">
+          <div class="row align-items-center">
+            <div class="col-12 col-lg-5 col-xl-6 mb-4 mb-lg-0">
+              <div class="text-block__picture mx-auto mx-lg-0">
+                <img class="text-block__image" src="img/page/main/about/photo.jpg" alt="">
+              </div>
+              <!-- /.about__picture -->
+            </div>
+            <!-- /.col-6 -->
+
+            <div class="col-12 col-lg-7 col-xl-6">
+              <div class="text-block__content">
+                <h2 class="text-block__title">
+                  Логопед
+                </h2>
+
+                <p class="text-block__text section__text">
+                  Индивидуальные занятия <br>
+                  Рекомендуемый возраст – определяется после индивидуальной диагностики
+                </p>
+
+                <p class="text-block__text section__text">
+                  <strong>Время проведения</strong> <br>
+                  обговаривается с педагогом
+                </p>
+
+                <p class="text-block__text section__text">
+                  <strong>Стоимость абонемента</strong> из 4 занятий – 5.200 руб. <br> Группа 5-6 человек
+                </p>
+
+                <div class="text-block__action">
+                  <button class="text-block__button button button-outline" data-micromodal-trigger="modal-callback">Записаться на пробное занятие</button>
+                </div>
+                <!-- /.about__action -->
+              </div>
+              <!-- /.about__content -->
+            </div>
+            <!-- /.col-6 -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.text-block__item -->
+
+        <div class="service__item text-block__item">
+          <div class="row align-items-center">
+            <div class="col-12 col-lg-5 col-xl-6 mb-4 mb-lg-0">
+              <div class="text-block__picture mx-auto mx-lg-0">
+                <img class="text-block__image" src="img/page/main/about/photo.jpg" alt="">
+              </div>
+              <!-- /.about__picture -->
+            </div>
+            <!-- /.col-6 -->
+
+            <div class="col-12 col-lg-7 col-xl-6">
+              <div class="text-block__content">
+                <h2 class="text-block__title">
+                  Музыкальная студия
+                </h2>
+
+                <p class="text-block__text section__text">
+                  Индивидуальные занятия <br>
+                  Рекомендуемый возраст – от 4 лет
+                </p>
+
+                <p class="text-block__text section__text">
+                  <strong>Время проведения</strong> <br>
+                  Четверг
+                </p>
+
+                <p class="text-block__text section__text">
+                  <strong>Стоимость абонемента</strong> из 4 занятий – 5.200 руб. <br> Группа 5-6 человек
+                </p>
+
+                <div class="text-block__action">
+                  <button class="text-block__button button button-outline" data-micromodal-trigger="modal-callback">Записаться на пробное занятие</button>
+                </div>
+                <!-- /.about__action -->
+              </div>
+              <!-- /.about__content -->
+            </div>
+            <!-- /.col-6 -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.text-block__item -->
+      </div>
+      <!-- /.service -->
     </div>
     <!-- /.container -->
   </section>
