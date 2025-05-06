8f0c53a82f59bbb1210c79d55785a55790e1aed9
acr0matic <work.acr0matic@gmail.com>
2025-05-06 23:06:47 +0300

    feat: страница "образование"

---
+++ src/education.php
@@ -0,0 +1,154 @@
+<?php include_once('partials/header.php') ?>
+
+<!-- Основной контент -->
+<main id="education-page">
+  <section id="cp-education" class="section">
+    <div class="container">
+      <div class="education">
+        <h1 class="studios__title page__title">Образование</h1>
+
+        <div class="education__table">
+          <div class="education__row">
+            <div class="education__col">
+              <p>Информация о реализуемых уровнях образования</p>
+            </div>
+            <!-- /.education__col -->
+
+            <div class="education__col">
+              <p><span>Дополнительное образование детей и взрослых</span></p>
+            </div>
+            <!-- /.education__col -->
+          </div>
+          <!-- /.education__row -->
+
+          <div class="education__row">
+            <div class="education__col">
+              <p>Информация о формах обучения</p>
+            </div>
+            <!-- /.education__col -->
+
+            <div class="education__col">
+              <p><span>Очно</span></p>
+            </div>
+            <!-- /.education__col -->
+          </div>
+          <!-- /.education__row -->
+
+          <div class="education__row">
+            <div class="education__col">
+              <p>Информация о нормативных сроках обучения</p>
+            </div>
+            <!-- /.education__col -->
+
+            <div class="education__col">
+              <p><span>Дополнительное образование детей и взрослых: от 5 часов до 320 часов.</span></p>
+            </div>
+            <!-- /.education__col -->
+          </div>
+          <!-- /.education__row -->
+
+          <div class="education__row">
+            <div class="education__col">
+              <p>Информация о сроке действия государственной аккредитации образовательной программы (при наличии государственной аккредитации)</p>
+            </div>
+            <!-- /.education__col -->
+
+            <div class="education__col">
+              <p><span>не имеется</span></p>
+            </div>
+            <!-- /.education__col -->
+          </div>
+          <!-- /.education__row -->
+
+          <div class="education__row">
+            <div class="education__col">
+              <p>Информация о численности обучающихся по реализуемым образовательным программам за счёт бюджетных ассигнований федерального бюджета, бюджетов субъектов Российской Федерации, местных бюджетов</p>
+            </div>
+            <!-- /.education__col -->
+
+            <div class="education__col">
+              <p><span>не имеется</span></p>
+            </div>
+            <!-- /.education__col -->
+          </div>
+          <!-- /.education__row -->
+
+          <div class="education__row">
+            <div class="education__col">
+              <p>Информация о численности обучающихся и по договорам об образовании за счёт средств физических и (или) юридических лиц</p>
+            </div>
+            <!-- /.education__col -->
+
+            <div class="education__col">
+              <p><span>не имеется</span></p>
+            </div>
+            <!-- /.education__col -->
+          </div>
+          <!-- /.education__row -->
+
+          <div class="education__row">
+            <div class="education__col">
+              <p>Информация о языках, на которых осуществляется образование (обучение)</p>
+            </div>
+            <!-- /.education__col -->
+
+            <div class="education__col">
+              <p><span>Обучение осуществляется на русском языке.</span></p>
+            </div>
+            <!-- /.education__col -->
+          </div>
+          <!-- /.education__row -->
+
+          <div class="education__row">
+            <div class="education__col">
+              <p>Информация о направлениях и результатах научной (научно - исследовательской) деятельности и научно- исследовательской базе для её осуществления</p>
+            </div>
+            <!-- /.education__col -->
+
+            <div class="education__col">
+              <p><span>не имеется</span></p>
+            </div>
+            <!-- /.education__col -->
+          </div>
+          <!-- /.education__row -->
+
+          <div class="education__row">
+            <div class="education__col">
+              <p>Информация о результатах приёма по каждому направлению подготовки или специальности высшего образования с различными условиями приёма (на места, финансируемые за счёт бюджетных ассигнований федерального бюджета, бюджетов субъектов Российской Федерации, местных бюджетов, по договорам об образовании за счёт средств физических и (или) юридических лиц) с указанием средней суммы набранных баллов по всем вступительным испытаниям</p>
+            </div>
+            <!-- /.education__col -->
+
+            <div class="education__col">
+              <p><span>не имеется</span></p>
+            </div>
+            <!-- /.education__col -->
+          </div>
+          <!-- /.education__row -->
+
+          <div class="education__row">
+            <div class="education__col">
+              <p>Информация о результатах перевода, восстановления и отчисления</p>
+            </div>
+            <!-- /.education__col -->
+
+            <div class="education__col">
+              <p><span>не имеется</span></p>
+            </div>
+            <!-- /.education__col -->
+          </div>
+          <!-- /.education__row -->
+        </div>
+        <!-- /.education__table -->
+
+        <div class="education__action">
+          <a href="" class="button button-primary">Скачать</a>
+        </div>
+        <!-- /.education__action -->
+      </div>
+      <!-- /.education -->
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once('partials/footer.php') ?>
